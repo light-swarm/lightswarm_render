190fab4644c5043dc24a044e14e91b390a6e07f6
Charles Rose <crose24@gmail.com>
2014-03-30 19:49:44 -0400

    initial capability

---
+++ scripts/renderer.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+
+import rospy
+import cv2
+import cv2.cv as cv
+import numpy as np
+
+import yaml
+
+from lightswarm_core.msg import Penumbras
+from lightswarm_core.msg import Shadow
+from lightswarm_core.msg import Polygon
+from lightswarm_core.msg import Point
+from lightswarm_core.msg import World
+from lightswarm_core.msg import Boid
+
+
+WORLD_LIMIT = 100
+CONFIG_FILENAME = '/home/ch21133/catkin_ws/src/lightswarm_render/scripts/renderer_config.yaml'
+
+
+class Renderer:
+    def __init__(self):
+        self.node_name = 'renderer'
+        
+        self.read_in_config(CONFIG_FILENAME)
+        
+        self.create_homograpy()
+        
+        self.create_displays()
+
+        self.sub_pen = rospy.Subscriber('/penumbras', Penumbras, self.penumbras_callback)
+        self.sub_world = rospy.Subscriber('/world', World, self.world_callback)
+ 
+ 
+    def read_in_config(self, filename):
+        f = open(filename)
+        config_map = yaml.safe_load(f)
+        f.close()
+        
+        self.proj_coord = config_map.get('projector_coordinates')
+        self.proj_res = config_map.get('projector_resolution')
+        
+        
+    def create_homograpy(self):
+        proj_pixels = np.float32([[0, 0], 
+            [0, self.proj_res[0]], 
+            [self.proj_res[1], self.proj_res[0]], 
+            [self.proj_res[1], 0]])
+            
+        self.homog, mask = cv2.findHomography(np.float32(self.proj_coord), proj_pixels, 0)
+        
+        
+    def create_displays(self):
+        #cv2.namedWindow('proj_1', cv2.WINDOW_NORMAL)
+        #cv2.setWindowProperty('proj_1', cv2.WND_PROP_FULLSCREEN, cv.CV_WINDOW_FULLSCREEN)
+        self.image = np.ones([self.proj_res[1], self.proj_res[0], 3], np.uint8)
+        self.image *= 255;
+        #cv2.imshow('proj_1', self.image)
+        #cv2.waitKey(30)
+
+        
+    def penumbras_callback(self, penumbras):
+        pass
+        
+        
+    def world_callback(self, world):
+        rospy.loginfo('got world')
+        boid_loc = []
+        boid_colors = []
+        boid_thetas = []
+        
+        for boid in world.boids:
+            boid_loc.append([boid.location.y, boid.location.x])
+            boid_colors.append(boid.color)
+            boid_thetas.append(boid.theta)
+            
+        loc_world = np.float32([ boid_loc ]).reshape(-1,1,2)
+        loc_pix = cv2.perspectiveTransform(loc_world , self.homog)
+        
+        #cv2.namedWindow('proj_1', cv2.WINDOW_NORMAL)
+        #cv2.imshow('proj_1', self.image)
+        #cv2.waitKey(30)
+        
+        #cv.NamedWindow('proj_1', 1)
+        #cv.ShowImage('proj_1', cv.fromarray(self.image))
+        #cv.WaitKey(3)
+        
+        self.update_image(loc_pix, boid_colors, boid_thetas)
+        
+        self.draw()
+        
+        
+    def update_image(self, loc_pix, boid_colors, boid_thetas):
+        for i in range(len(boid_thetas)):
+            loc = loc_pix[i][0]
+            #color = boid_colors[i]
+            #boid_thetas[i]
+            poly = np.array([ [loc[1]+5, loc[0]+5], 
+                [loc[1]-5, loc[0]+5],
+                [loc[1]-5, loc[0]-5],
+                [loc[1]+5, loc[0]-5] ], np.int32)
+            cv2.fillConvexPoly(self.image, poly, [255, 0, 0])
+
+        
+        
+        
+    def draw(self):
+        cv2.namedWindow('proj_1', cv2.WINDOW_NORMAL)
+        cv2.setWindowProperty('proj_1', cv2.WND_PROP_FULLSCREEN, cv.CV_WINDOW_FULLSCREEN)
+        cv2.imshow('proj_1', self.image)
+        cv2.waitKey(30)
+
+
+    def test(self):
+        im_fname = '/home/ch21133/Downloads/OpenCV/opencv-2.4.8/samples/cpp/baboon.jpg'
+        
+        #im2 = cv2.imread(im_fname, cv.CV_LOAD_IMAGE_COLOR)
+        
+        #cv2.imshow('proj_1', im2)
+        #cv2.waitKey()
+        #cv2.destroyAllWindows()
+        
+        
+        
+        #im3 = cv2.perspectiveTransform(im2, self.homog)
+
+
+def main():
+    renderer = Renderer()
+    
+    rospy.init_node(renderer.node_name, anonymous=True)
+    
+    rospy.spin()
+    
+
+if __name__ == '__main__':
+    main()
+    
+    
+    
+    
+    
