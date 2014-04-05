324e484912ce89f6e2a59992132c24c634eab0c2
Charles Rose <crose24@gmail.com>
2014-04-05 18:43:40 -0400

    added simple shadow overlay

--- scripts/renderer.py
+++ scripts/renderer.py
@@ -16,7 +16,7 @@ from lightswarm_core.msg import Boid
 
 
 WORLD_LIMIT = 100
-CONFIG_FILENAME = '/home/ch21133/catkin_ws/src/lightswarm_render/scripts/renderer_config.yaml'
+CONFIG_FILENAME = 'src/lightswarm_render/scripts/renderer_config.yaml'
 
 
 class Renderer:
@@ -55,8 +55,8 @@ class Renderer:
         cv2.namedWindow('proj_1', cv2.WINDOW_NORMAL)
         #cv2.setWindowProperty('proj_1', cv2.WND_PROP_FULLSCREEN, cv.CV_WINDOW_FULLSCREEN)
         self.reset_image()
-        cv2.imshow('proj_1', self.image)
-        cv2.waitKey(30)
+        self.shadows = []
+        self.draw()
         
     def reset_image(self):
         self.image = np.zeros([self.proj_res[1], self.proj_res[0], 3], np.uint8)
@@ -64,6 +64,12 @@ class Renderer:
         
     def penumbras_callback(self, penumbras):
         
+        self.shadows = []
+        for shadow in penumbras.projector_shadows:
+            poly = []
+            for point in shadow.polygon.points:
+                poly.append([point.y, point.x])
+            self.shadows.append(poly)
         
         
     def world_callback2(self, world):
@@ -75,11 +81,9 @@ class Renderer:
             poly = self.create_boid_poly([boid.location.y, boid.location.x], boid.theta)
             boid_colors.append(boid.color)
             poly = np.float32([ poly ]).reshape(-1,1,2)
-            boid_pix.append(cv2.perspectiveTransform(poly , self.homog))
-            
+            boid_pix.append(cv2.perspectiveTransform(poly , self.homog)) 
         
         self.update_image2(boid_pix, boid_colors)
-        
         self.draw()
         
         
@@ -97,73 +101,23 @@ class Renderer:
     def update_image2(self, boid_pix, boid_colors):
         self.reset_image()
         for i in range(len(boid_pix)):
-            #color = boid_colors[i]
             poly = np.int32(boid_pix[i]).reshape(1,-1,2)
-            cv2.fillConvexPoly(self.image, np.fliplr(poly[0]), [255, 255, 255])
+            cv2.fillConvexPoly(self.image, np.fliplr(poly[0]), boid_colors[i])
+            
+        for poly in self.shadows:
+            poly = np.float32([ poly ]).reshape(-1,1,2)
+            pix = cv2.perspectiveTransform(poly , self.homog)
+            pix = np.int32(pix).reshape(1,-1,2)
+            cv2.fillConvexPoly(self.image, np.fliplr(pix[0]), [0, 0, 0])
 
 
         
     def draw(self):
         #cv2.namedWindow('proj_1', cv2.WINDOW_NORMAL)
-        #cv2.setWindowProperty('proj_1', cv2.WND_PROP_FULLSCREEN, cv.CV_WINDOW_FULLSCREEN)
+        cv2.setWindowProperty('proj_1', cv2.WND_PROP_FULLSCREEN, cv.CV_WINDOW_FULLSCREEN)
         cv2.imshow('proj_1', self.image)
         a = cv2.waitKey(30)
         
-        
-    def world_callback(self, world):
-        rospy.loginfo('got world')
-        boid_loc = []
-        boid_colors = []
-        boid_thetas = []
-        
-        for boid in world.boids:
-            boid_loc.append([boid.location.y, boid.location.x])
-            boid_colors.append(boid.color)
-            boid_thetas.append(boid.theta)
-            
-        loc_world = np.float32([ boid_loc ]).reshape(-1,1,2)
-        loc_pix = cv2.perspectiveTransform(loc_world , self.homog)
-        
-        #cv2.namedWindow('proj_1', cv2.WINDOW_NORMAL)
-        #cv2.imshow('proj_1', self.image)
-        #cv2.waitKey(30)
-        
-        #cv.NamedWindow('proj_1', 1)
-        #cv.ShowImage('proj_1', cv.fromarray(self.image))
-        #cv.WaitKey(3)
-        
-        self.update_image(loc_pix, boid_colors, boid_thetas)
-        
-        self.draw()
-        
-        
-    def update_image(self, loc_pix, boid_colors, boid_thetas):
-        for i in range(len(boid_thetas)):
-            loc = loc_pix[i][0]
-            #color = boid_colors[i]
-            #boid_thetas[i]
-            poly = np.array([ [loc[1]+5, loc[0]+5], 
-                [loc[1]-5, loc[0]+5],
-                [loc[1]-5, loc[0]-5],
-                [loc[1]+5, loc[0]-5] ], np.int32)
-            print poly
-            cv2.fillConvexPoly(self.image, poly, [255, 0, 0])
-
-        
-
-
-    def test(self):
-        im_fname = '/home/ch21133/Downloads/OpenCV/opencv-2.4.8/samples/cpp/baboon.jpg'
-        
-        #im2 = cv2.imread(im_fname, cv.CV_LOAD_IMAGE_COLOR)
-        
-        #cv2.imshow('proj_1', im2)
-        #cv2.waitKey()
-        #cv2.destroyAllWindows()
-        
-        
-        
-        #im3 = cv2.perspectiveTransform(im2, self.homog)
 
 
 def main():
