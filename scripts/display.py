008f52e0730f801499ff2c0ffb77d0281f4c69b3
Charles Rose <crose24@gmail.com>
2014-04-09 18:04:02 -0400

    added display class

---
+++ scripts/display.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import rospy
+import cv2
+import cv2.cv as cv
+import numpy as np
+
+class Display:
+
+    def setup(self):       
+        cv2.namedWindow('proj_0', cv2.WINDOW_NORMAL)
+        #cv2.setWindowProperty(self.proj_names[i], cv2.WND_PROP_FULLSCREEN, cv.CV_WINDOW_FULLSCREEN)
+        
+    def draw(self, image):
+        cv2.imshow('proj_0', image)
+        cv2.waitKey(30)
+        
