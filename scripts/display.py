bfb57b168fc083161a98d0690c5f75c678027589
Charles Rose <crose24@gmail.com>
2014-04-12 18:04:25 -0400

    added initial projector responsibility

--- scripts/display.py
+++ scripts/display.py
@@ -7,11 +7,12 @@ import numpy as np
 
 class Display:
 
-    def setup(self):       
+    def setup(self, fullscreen):       
         cv2.namedWindow('proj_0', cv2.WINDOW_NORMAL)
-        #cv2.setWindowProperty(self.proj_names[i], cv2.WND_PROP_FULLSCREEN, cv.CV_WINDOW_FULLSCREEN)
+        if fullscreen:
+            cv2.setWindowProperty(self.proj_names[i], cv2.WND_PROP_FULLSCREEN, cv.CV_WINDOW_FULLSCREEN)
         
     def draw(self, image):
         cv2.imshow('proj_0', image)
-        cv2.waitKey(30)
+        cv2.waitKey(1)
         
--- scripts/renderer.py
+++ scripts/renderer.py
@@ -4,9 +4,12 @@ import rospy
 import cv2
 import cv2.cv as cv
 import numpy as np
-import shapely
-
 import yaml
+
+import shape
+from shapely.geometry import Polygon as ShpPolygon
+from shapely.geometry import MultiPolygon as ShpMultiPolygon
+from shapely.geometry import Point as ShpPoint
 
 from lightswarm_core.msg import Penumbras
 from lightswarm_core.msg import Shadow
@@ -17,21 +20,23 @@ from lightswarm_core.msg import Boid
 
 from display import Display
 
-
 CONFIG_FILENAME = 'src/lightswarm_core/params/config.yaml'
-
 
 class Renderer:
     def __init__(self):
         self.node_name = 'renderer'
         
+        self.proj_id_2idx = {'first':0, 'second':1, 'third':2, 'fourth':3}
+        
         config_fname = rospy.get_param('config_file', CONFIG_FILENAME)
+        fullscreen = rospy.get_param('~fullscreen', False)
         self.read_in_config(config_fname)
         self.create_homograpy()
-        self.create_displays2()
+        self.calc_resp([])
+        self.create_displays2(fullscreen)
 
-        self.sub_pen = rospy.Subscriber('/penumbras', Penumbras, self.penumbras_callback)
-        self.sub_world = rospy.Subscriber('/world', World, self.world_callback2)
+        self.sub_pen = rospy.Subscriber('/penumbras', Penumbras, self.penumbras_callback2)
+        self.sub_world = rospy.Subscriber('/world', World, self.world_callback2, queue_size = 1)
  
  
     def read_in_config(self, filename):
@@ -44,60 +49,70 @@ class Renderer:
         self.proj_res = config_map.get('projector_resolution')
         
         
-    def create_homograpy(self):
+    def create_homograpy(self): #creates homography transformations between coordinates and projector pixels
         proj_pixels = np.float32([[0, 0], 
             [0, self.proj_res[0]], 
             [self.proj_res[1], self.proj_res[0]], 
             [self.proj_res[1], 0]])
         
         self.homog = []
-        for i in range(self.num_proj):
+        for i in range(self.num_proj): #one for each projector
             temp_homog, mask = cv2.findHomography(np.float32(self.proj_coord[i]), proj_pixels, 0)
             self.homog.append(temp_homog)
 
 
-    def create_displays2(self):
+    def create_displays2(self, fullscreen): #create display objects for each projector image
         self.reset_image()
         self.shadows = []
         
         self.display = Display()
-        self.display.setup()
+        self.display.setup(fullscreen)
         self.draw()
         
         
-    def reset_image(self):
+    def reset_image(self): #reset internal image for projectors
         self.image = []
         for i in range(self.num_proj):
             self.image.append(np.zeros([self.proj_res[1], self.proj_res[0], 3], np.uint8))
 
         
-    def penumbras_callback(self, penumbras):
-        
-        self.shadows = []
-        for shadow in penumbras.projector_shadows:
+    def penumbras_callback2(self, penumbras): #update images with new responsibilities for projectors
+        rospy.loginfo('got new shadows')
+        self.calc_resp(penumbras.projector_shadows)
+        self.update_image2()
+
+    
+    
+    def calc_resp(self, projector_shadows):
+    
+        shadow_union = [ShpPolygon()]*self.num_proj
+        for shadow in projector_shadows:
+            proj_idx = self.proj_id_2idx[shadow.projector_id]
+            
             poly = []
             for point in shadow.polygon.points:
                 poly.append([point.y, point.x])
-            self.shadows.append(poly)
+                
+            shadow_union[proj_idx] = shadow_union[proj_idx].union(ShpPolygon(poly))
+            
+            
+        new_resp = [None]*self.num_proj
+        prev_coverage = ShpPolygon()
         
+        for i in range(self.num_proj):
+            curr_poly = ShpPolygon(self.proj_coord[i])
+            curr_poly = curr_poly.difference(shadow_union[i])
+            new_coverage = curr_poly.difference(prev_coverage)
+            new_resp[i] = new_coverage
+            prev_coverage.union(curr_poly)
+            
+        self.resp = new_resp
+   
         
-    def world_callback2(self, world):
+    def world_callback2(self, world): #update images with new boid locations
         rospy.loginfo('got world')
-        boid_pix0 = []
-        boid_pix1 = []
-        boid_colors = []
-        
-        for boid in world.boids:
-            poly = self.create_boid_poly([boid.location.y, boid.location.x], boid.theta)
-            boid_colors.append(boid.color)
-            poly = np.float32([ poly ]).reshape(-1,1,2)
-            boid_pix0.append(cv2.perspectiveTransform(poly, self.homog[0]))
-            boid_pix1.append(cv2.perspectiveTransform(poly, self.homog[1])) 
-        
-        self.reset_image()
-        self.update_image2(boid_pix0, boid_colors, 0)
-        self.update_image2(boid_pix1, boid_colors, 1)
-        self.draw()
+        self.world = world
+        self.update_image2()
         
         
     def create_boid_poly(self, loc, theta):
@@ -111,37 +126,34 @@ class Renderer:
         return poly
         
         
-    def update_image2(self, boid_pix, boid_colors, proj_idx):
+    def update_image2(self): #updates images with boid locations and responsibility vectors
+        self.reset_image()
         
-        for i in range(len(boid_pix)):
-            poly = np.int32(boid_pix[i]).reshape(1,-1,2)
-            cv2.fillConvexPoly(self.image[proj_idx], np.fliplr(poly[0]), boid_colors[i])
+        for boid in self.world.boids:
+            poly = self.create_boid_poly([boid.location.y, boid.location.x], boid.theta)#create boid polygon in world coord
             
-        for poly in self.shadows:
-            poly = np.float32([ poly ]).reshape(-1,1,2)
-            pix = cv2.perspectiveTransform(poly, self.homog[proj_idx])
-            pix = np.int32(pix).reshape(1,-1,2)
-            cv2.fillConvexPoly(self.image[proj_idx], np.fliplr(pix[0]), [0, 0, 0])
+            #figure out which projector owns it
+            proj_own = -1
+            for i in range(self.num_proj):
+                if self.resp[i].contains(ShpPoint(boid.location.y, boid.location.x)):
+                    proj_own = i
+                    break
+                    
+            #transform to proj coordinates
+            if (proj_own >= 0):
+                poly = np.float32([ poly ]).reshape(-1,1,2)
+                boid_pix = cv2.perspectiveTransform(poly, self.homog[proj_own])            
+                boid_pix = np.int32(boid_pix).reshape(1,-1,2)
+                cv2.fillConvexPoly(self.image[proj_own], np.fliplr(boid_pix[0]), boid.color)
+
+        self.draw()
 
     def draw(self):
         combined_image = self.combine_image(self.image[0], self.image[1])
-        combined_image = self.combine_image(combined_image, self.image[1])
         self.display.draw(combined_image)
             
     def combine_image(self, image1, image2):
         return np.hstack((image1, image2))
-
-    def create_displays_old(self):
-        cv2.namedWindow('proj_1', cv2.WINDOW_NORMAL)
-        #cv2.setWindowProperty('proj_1', cv2.WND_PROP_FULLSCREEN, cv.CV_WINDOW_FULLSCREEN)
-        self.reset_image()
-        self.shadows = []
-        self.draw()
-        
-    def draw_old(self):
-        cv2.imshow('proj_1', self.image)
-        a = cv2.waitKey(30)
-        
 
 
 def main():
