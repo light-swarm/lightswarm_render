6d152370532699f23a6bf13d93436849341dd7ad
Charles Rose <crose24@gmail.com>
2014-04-14 12:36:49 -0400

    fixed error in fullscreen window command

--- scripts/display.py
+++ scripts/display.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 
-import rospy
 import cv2
 import cv2.cv as cv
-import numpy as np
 
 class Display:
 
     def setup(self, fullscreen):       
         cv2.namedWindow('proj_0', cv2.WINDOW_NORMAL)
         if fullscreen:
-            cv2.setWindowProperty(self.proj_names[i], cv2.WND_PROP_FULLSCREEN, cv.CV_WINDOW_FULLSCREEN)
+            cv2.setWindowProperty('proj_0', cv2.WND_PROP_FULLSCREEN, cv.CV_WINDOW_FULLSCREEN)
         
     def draw(self, image):
         cv2.imshow('proj_0', image)
